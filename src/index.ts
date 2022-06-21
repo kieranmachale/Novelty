6526f7a914b569a4607d213d86cb44de34f41bcc
kieranmachale <c16777089@mytudublin.ie>
2022-06-21 21:46:37 +0100

    Initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,109 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+import { v4 as uuidV4 } from "uuid"
 
-import confetti from 'canvas-confetti';
+type Task = {
+  id: string
+  title: string
+  ISBN: string
+  imageLink: string
+  completed: boolean
+  createdAt: Date
+}
 
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+const list = document.querySelector<HTMLUListElement>("#list")
+const form = document.getElementById("new-task-form") as HTMLFormElement | null
+const input = document.querySelector<HTMLInputElement>("#new-task-title")
+const tasks: Task[] = loadTasks()
+tasks.forEach(addListItem)
+
+const clearBtn = document.querySelector("#clear")
+clearBtn?.addEventListener("click", function(){
+  clearTasks()
+})
+
+form?.addEventListener("submit", e => {
+  e.preventDefault()
+
+  if (input?.value == "" || input?.value == null) return
+
+  // Fetching book images
+  var isbn = input.value
+  var url = 'https://www.googleapis.com/books/v1/volumes?q=isbn:' + isbn;
+
+  fetch(url)
+    .then(res => {
+      return res.json();
+    })
+    .then(data => {
+      const newTask: Task = {
+        id: uuidV4(),
+        title: data.items[0].volumeInfo.title,
+        ISBN: input.value,
+        imageLink: data.items[0].volumeInfo.imageLinks.thumbnail,
+        completed: false,
+        createdAt: new Date(),
+      }
+      tasks.push(newTask)
+      saveTasks()
+    
+      addListItem(newTask)
+      input.value = ""
+        
+    })
+    .catch(err => {
+        console.log(err.message || "Error occurred while fetching book info");
+    });
+})
+
+function addListItem(task: Task) {
+  const item = document.createElement("li")
+  const cover = document.createElement("img")
+  const label = document.createElement("label")
+  const checkbox = document.createElement("input")
+  checkbox.addEventListener("change", () => {
+    task.completed = checkbox.checked
+    saveTasks()
+  })
+  checkbox.type = "checkbox"
+  checkbox.checked = task.completed
+
+  cover.src = task.imageLink
+
+  label.append(checkbox, task.title)
+  /*item.append(label)*/
+  item.append(cover)
+  cover.addEventListener("click", () => {
+    item.remove()
+    var arr = JSON.parse(localStorage["TASKS"])
+    var newArray = arr.filter((el: { ISBN: any }) => el.ISBN !== task.ISBN)
+    localStorage.setItem('TASKS', JSON.stringify(newArray));
+  })
+
+  cover.addEventListener("mouseover", () => {
+    console.log(task)
+  })
+  /*
+  cover.addEventListener("mouseout", () => {
+    setTimeout(function () {
+      item.removeChild(label)  
+  }, 2000);
+    
+  })*/
+
+  list?.append(item)
+
+}
+
+function saveTasks() {
+  localStorage.setItem("TASKS", JSON.stringify(tasks))
+}
+
+function loadTasks(): Task[] {
+  const taskJSON = localStorage.getItem("TASKS")
+  if (taskJSON == null) return []
+  return JSON.parse(taskJSON)
+}
+
+function clearTasks() {
+  localStorage.clear();
+  list!.innerHTML = " "
+}
